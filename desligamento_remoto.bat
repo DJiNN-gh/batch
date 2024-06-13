5d91b35a923f59b0fbef0e30d9317a327cefd13f
Allan Lopes <allaneumermo@gmail.com>
2024-06-13 13:40:18 -0300

    Update desligamento_remoto.bat

--- desligamento_remoto.bat
+++ desligamento_remoto.bat
@@ -4,3 +4,5 @@ net use \\192.168.1.1 /user:Administrador *
 
 REM Comando 'shutdown' para iniciar processo de encerramento do dispositivo, parametro -s para desligamento (shutdown), -t para tempo em segundos (time), -m para dispositivo remoto (machine) e -c (commentary) para uma mensagem curta que aparecera na tela do dispositivo
 shutdown /s /t 60 /m \\192.168.1.1 /c "Desligamento remoto forçado pelo administrador (60s)"
+
+exit
