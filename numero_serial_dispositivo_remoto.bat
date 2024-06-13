87d23b169577af3886b8508d403d306c6e326e3e
Allan Lopes <allaneumermo@gmail.com>
2024-06-13 17:17:14 -0300

    Update numero_serial_dispositivo_remoto.bat

--- numero_serial_dispositivo_remoto.bat
+++ numero_serial_dispositivo_remoto.bat
@@ -1,9 +1,18 @@
-REM Utilizando o utilitario 'WMIC' para requisitar informacoes de um dispositivo remoto
-wmic
+@echo off
 
-REM Sao passados os parametros do endereco do computador (node) por nome ou IP, usuario (user) e senha (password) sempre entre aspas duplas ("") e precedidos por dois pontos na chamada (:)
-/node:"192.168.1.1" /user:"Administrador"
+REM Guardando o caminho/endereco
+echo Insira o endereco ou nome do dispositivo:
 
-REM Acessando o ambiente da BIOS e usando o metodo GET para requisitar o numero serial do dispositivo
+set /p end_str=
 
-bios get serialnumber
+REM Guardando o usuario
+echo Insita o nome do usuario:
+
+set /p usuario_str=
+
+REM Utilizando o utilitario 'WMIC' para requisitar informacoes de um dispositivo remoto no ambiente da BIOS usando o metodo GET
+REM Sao passados os parametros do endereco do computador (node) por nome ou IP e usuario (user) sempre entre aspas duplas ("") e precedidos por dois pontos na chamada (:)
+REM Um prompt para senha sera iniciado quando o argumento (password) nao for passado no comando
+wmic /node:"%end_str%" /user:"%usuario_str%" /output:"c:\users\public\documents\%end_str%.txt" bios get serialnumber
+
+exit
