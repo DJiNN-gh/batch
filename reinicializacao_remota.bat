f8f9fe9e66db007d811d445977a3447e61bff505
Allan Lopes <allaneumermo@gmail.com>
2024-06-13 17:27:29 -0300

    Update reinicializacao_remota.bat

--- reinicializacao_remota.bat
+++ reinicializacao_remota.bat
@@ -1,5 +1,20 @@
+@echo off
+
+REM Guardando o caminho/endereco
+echo Insira o endereco ou nome do dispositivo:
+
+set /p end_str=
+
+REM Guardando o usuario
+
+echo Insira o nome do usuario:
+
+set /p usuario_str=
+
+REM Adquirindo privilegio de administrador para a reinicializacao
 net use \\192.168.1.1 /user:Administrador *
 
+REM Passando parametros de reinicializacao (-r), tempo (-t), maquina remota (-m) e comentario (-c)
 shutdown /r /t 60 /m \\192.168.1.1 /c "Reinicialização remota forçada pelo administrador (60s)"
 
 exit
